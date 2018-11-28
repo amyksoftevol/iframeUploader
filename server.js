e8c8b86dffb650656f8c6c302a1126889d27e835
amyksoftevol <amyk.softevol@gmail.com>
2018-11-28 17:34:43 +0200

    fixes for heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/iframe-uploader'));
+app.use(express.static('./dist/iframeUploader'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/iframe-uploader/index.html'));
+res.sendFile(path.join(__dirname,'/dist/iframeUploader/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
