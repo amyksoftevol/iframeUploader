b6b676c38bc78ccb4fb0cffad91332189788d2fe
Andrii Mykolaichyk <andrii.mykolaichyk@gmail.com>
2018-11-29 12:14:41 +0200

    add parent message listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iframeUploader';
 
+  ngOnInit() {
+    window.addEventListener('message', event => {
+      console.log('Message from PARENT: ', event);
+    });
+  }
+
   uploadChoosedVideo(elemIdName: string) {
-    const fileInput = document.getElementById(elemIdName);
+    const fileInputElement = document.getElementById(elemIdName);
 
     const processVideo = event => {
-        fileInput.removeEventListener('change', processVideo);
+      fileInputElement.removeEventListener('change', processVideo);
         console.log('UPLOADED FILE: ', event.target['files']);
         // if (!videoUpload) {
         //     this.videoUploadService.addFilesAndUpload(event.target['files']);
         // } else {
         //     this.videoUploadService.addFileAndUpload(event.target['files'], videoUpload);
         // }
-        // fileInput['value'] = '';
+        // fileInputElement['value'] = '';
     };
 
     const preventClickPropagation = event => {
         event.stopPropagation();
-        fileInput.removeEventListener('click', preventClickPropagation);
+        fileInputElement.removeEventListener('click', preventClickPropagation);
     };
 
-    fileInput.addEventListener('change', processVideo);
-    fileInput.addEventListener('click', preventClickPropagation);
-    fileInput.click();
+    fileInputElement.addEventListener('change', processVideo);
+    fileInputElement.addEventListener('click', preventClickPropagation);
+    fileInputElement.click();
   }
+
+  // // addEventListener support for IE8
+  // bindEvent(element, eventName, eventHandler) {
+  //   if (element.addEventListener) {
+  //       element.addEventListener(eventName, eventHandler, false);
+  //   } else if (element.attachEvent) {
+  //       element.attachEvent('on' + eventName, eventHandler);
+  //   }
+  // }
+
+  // // removeEventListener support for IE8
+  // unbindEvent(element, eventName, eventHandler) {
+  //   if (element.removeEventListener) {
+  //       element.removeEventListener(eventName, eventHandler);
+  //   } else if (element.detachEvent) {
+  //       element.detachEvent('on' + eventName, eventHandler);
+  //   }
+  // }
 
 }
