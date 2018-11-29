540ea1e1f0e8e6f9d6ece3c09b109b81bea34e5e
Andrii Mykolaichyk <andrii.mykolaichyk@gmail.com>
2018-11-29 12:46:46 +0200

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'iframeUploader';
 
-  ngOnInit() {
+  constructor() {
     window.addEventListener('message', event => {
-      console.log('Message from PARENT: ', event);
+      console.log('Message from PARENT: ', event.data);
     });
+  }
+
+  ngOnInit() {
   }
 
   uploadChoosedVideo(elemIdName: string) {
