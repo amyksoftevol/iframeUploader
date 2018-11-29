e2a71b44bd43eafeccffce37419362b860182090
amyksoftevol <amyk.softevol@gmail.com>
2018-11-29 10:31:51 +0200

    add file upload button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iframeUploader';
+
+  uploadChoosedVideo(elemIdName: string, videoUpload) {
+    const fileInput = document.getElementById(elemIdName);
+
+    const processVideo = event => {
+        fileInput.removeEventListener('change', processVideo);
+        console.log('UPLOADED FILE: ', event.target['files']);
+        // if (!videoUpload) {
+        //     this.videoUploadService.addFilesAndUpload(event.target['files']);
+        // } else {
+        //     this.videoUploadService.addFileAndUpload(event.target['files'], videoUpload);
+        // }
+        // fileInput['value'] = '';
+    };
+
+    const preventClickPropagation = event => {
+        event.stopPropagation();
+        fileInput.removeEventListener('click', preventClickPropagation);
+    };
+
+    fileInput.addEventListener('change', processVideo);
+    fileInput.addEventListener('click', preventClickPropagation);
+    fileInput.click();
+  }
+
 }
